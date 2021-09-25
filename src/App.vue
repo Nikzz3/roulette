26f3fcbd0463bbec7cd875c75de0570e4f76c5bb
Nick Zimmermann <Nick.Zimmermann@swisscom.com>
2021-09-25 12:17:24 +0200

    add Roulette

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="container">
+    <h1>Roulette</h1>
+    <form @submit.prevent="submit">
+      <div class="input-group mb-3"></div>
+      <div>
+        <input
+          type="number"
+          class="form-control mb-3"
+          placeholder="Einsatz..."
+          v-model="einsatz"
+          required
+        />
+        <input
+          type="number"
+          class="form-control mb-3"
+          placeholder="Höchsteinsatz..."
+          v-model="maxValue"
+          required
+        />
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
+    </form>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Winner Number</th>
+          <th scope="col">Current Money</th>
+          <th scope="col">Handle</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(number, index) in winnerNumber" :key="index">
+          <th scope="row">{{ index }}</th>
+          <td>{{ number.winner }}</td>
+          <td>{{ number.currentValue }}</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
+  name: 'App',
+  data() {
+    return {
+      einsatz: null,
+      maxValue: null,
+      winnerNumber: [],
+    };
+  },
+  methods: {
+    submit() {
+      console.log(this.einsatz);
+      console.log(this.maxValue);
+      for (var i = 0; i < 100; i++) {
+        const randomVal = this.getRandomInt();
+        this.winnerNumber.push({
+          winner: randomVal,
+          currentValue: (this.maxValue -= this.einsatz),
+        });
+        this.einsatz = this.einsatz * 2;
+      }
+    },
+
+    getRandomInt() {
+      return Math.floor(Math.random() * 36);
+    },
   },
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
